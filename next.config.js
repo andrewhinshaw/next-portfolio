abe3c781355c2c27b54c0e30ec0d2255d54d697e
Andrew Hinshaw <andrewthinshaw@gmail.com>
2023-08-20 19:51:24 -0700

    chore: cleanup for config and packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,2 @@
-/** @type {import('next').NextConfig} */
-const withVideos = require("next-videos");
-module.exports = withVideos();
 
-const nextConfig = {};
-module.exports = nextConfig;
+module.exports = {};
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,5 +17,9 @@ module.exports = {
       },
     },
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [
+    require('flowbite/plugin')({
+      charts: true,
+    })
+  ],
 }
